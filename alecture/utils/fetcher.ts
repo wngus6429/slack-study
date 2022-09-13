39da01981a6f8a26c950e774133c7571fb6c440b
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-09-13 21:00:45 +0900

    Update fetcher.ts

--- alecture/utils/fetcher.ts
+++ alecture/utils/fetcher.ts
@@ -4,11 +4,12 @@ import axios from 'axios';
 //? 안되는것을 해결해준다, get은 두번쨰, post는 3번째에 적는다.
 //! 백엔드에서 프론트로 쿠키생성도 되고 프론트에서 백엔드로 데이터도 전달이됨
 //! 쿠키는 백엔드가 만들어내는거고 백엔드에서 받아서 브라우저에 저장
-const fetcher = (url: string) =>
-  axios
+const fetcher = (url: string) => {
+  return axios
     .get(url, {
       withCredentials: true,
     })
     .then((response) => response.data);
+};
 
 export default fetcher;
