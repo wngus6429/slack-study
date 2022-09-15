dee24925d051e50b217ef888cb85d18919f79989
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-09-16 00:53:00 +0900

    update slack workspace update

--- alecture/layouts/Workspace/index.tsx
+++ alecture/layouts/Workspace/index.tsx
@@ -1,9 +1,20 @@
-import { Button } from '@pages/SignUp/styles';
+import {
+  Header,
+  RightMenu,
+  ProfileImg,
+  WorkspaceWrapper,
+  Workspaces,
+  Channels,
+  WorkspaceName,
+  Chats,
+  MenuScroll,
+} from '@layouts/Workspace/styles';
 import axios from 'axios';
 import React, { FC, useCallback } from 'react';
 import fetcher from '@utils/fetcher';
 import useSWR from 'swr';
 import { Redirect } from 'react-router';
+import gravatar from 'gravatar';
 
 // FC라는 타입안에 children이 들어있다.
 // children 안 쓰는곳은 VFC 로 하면된다.
@@ -23,10 +34,12 @@ const Workspace: FC = ({ children }) => {
         // 서버 처리는 나중에 함 OPTIMISTIC UI
         // 서버 처리 후 반영은 패시미스틱 UI (일반적으로 이거)
         // 여기 뒤에 true면 OPTIMISTIC이 된다. shouldRevalidate()
-        mutate(response.data, false);
+        console.log('리스폰', response);
+        mutate(false);
+        // mutate(response.data, false);
       });
   }, []);
-
+  console.log('로그아웃후', data);
   if (data === false) {
     console.log('data꾸에엑2', data);
     return <Redirect to="/login" />;
@@ -34,7 +47,22 @@ const Workspace: FC = ({ children }) => {
 
   return (
     <div>
-      <Button onClick={onLogout}>로그아웃</Button>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro' })} alt={data.ni}></ProfileImg>
+          </span>
+        </RightMenu>
+      </Header>
+      <button onClick={onLogout}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>MenuScroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
       {children}
     </div>
   );
